7e74f6f8f8906f86ffc840f7a98ae3af5e883013
Rafael Souza <rafaeljosegomes25@gmail.com>
2022-10-10 18:28:18 -0300

    created default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './screens/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   )
 }
