728b8b1ffcb2e174ab40ed918f988e93114717e6
Rafael Souza <rafaeljosegomes25@gmail.com>
2022-10-10 21:39:24 -0300

    styled banner

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,51 @@
+import { GridBanner, MenuInfo } from './styles'
+import { ShoppingCart, Package, Timer, Coffee } from 'phosphor-react'
+import { Container } from '../../components/Container'
+import mainImage from '../../assets/main-image.png'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <Container>
+      <GridBanner>
+        <MenuInfo>
+          <div className="title">
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <span>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </span>
+          </div>
+          <div className="mainInfoWithGap">
+            <div className="iconInfo">
+              <div className="icon cart">
+                <ShoppingCart size={20} />
+              </div>
+              <p>Compra simples e segura</p>
+            </div>
+            <div className="iconInfo">
+              <div className="icon package">
+                <Package size={20} />
+              </div>
+              <p>Embalagem mantém o café intacto</p>
+            </div>
+          </div>
+          <div className="mainInfo">
+            <div className="iconInfo">
+              <div className="icon timer">
+                <Timer size={20} />
+              </div>
+              <p>Entrega rápida e rastreada</p>
+            </div>
+            <div className="iconInfo">
+              <div className="icon coffee">
+                <Coffee size={20} />
+              </div>
+              <p>O café chega fresquinho até você</p>
+            </div>
+          </div>
+        </MenuInfo>
+        <img src={mainImage} alt="" />
+      </GridBanner>
+    </Container>
+  )
 }
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,137 @@
+import styled, { css } from 'styled-components'
+
+export const GridBanner = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 29.75rem;
+  gap: 3.5rem;
+  margin-top: 5rem;
+
+  img {
+    width: 29.75rem;
+    height: 22.5rem;
+  }
+`
+
+export const MenuInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  div.title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+  }
+
+  div.title h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 800;
+    font-size: 48px;
+    line-height: 62.4px;
+
+    ${(props) =>
+      css`
+        color: ${props.theme.baseTitle};
+      `}
+  }
+
+  div.title span {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 26px;
+
+    ${(props) =>
+      css`
+        color: ${props.theme.baseSubtitle};
+      `}
+  }
+
+  div.icon {
+    height: 2rem;
+    width: 2rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 50%;
+  }
+
+  div.iconInfo {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+  }
+
+  div.mainInfo {
+    display: flex;
+    align-items: center;
+    gap: 2.5rem;
+  }
+
+  div.mainInfoWithGap {
+    display: flex;
+    align-items: center;
+    gap: 2.5rem;
+    margin-top: 4rem;
+    margin-bottom: 1.25rem;
+  }
+
+  div.iconInfo p {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 20.8px;
+  }
+
+  div.cart {
+    ${(props) =>
+      css`
+        background-color: ${props.theme.yellowDark};
+      `}
+
+    ${(props) =>
+      css`
+        color: ${props.theme.white};
+      `}
+  }
+
+  div.timer {
+    ${(props) =>
+      css`
+        background-color: ${props.theme.yellow};
+      `}
+
+    ${(props) =>
+      css`
+        color: ${props.theme.white};
+      `}
+  }
+
+  div.package {
+    ${(props) =>
+      css`
+        background-color: ${props.theme.baseText};
+      `}
+
+    ${(props) =>
+      css`
+        color: ${props.theme.white};
+      `}
+  }
+
+  div.coffee {
+    ${(props) =>
+      css`
+        background-color: ${props.theme.purple};
+      `}
+
+    ${(props) =>
+      css`
+        color: ${props.theme.white};
+      `}
+  }
+`
