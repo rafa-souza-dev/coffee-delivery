03a67e9e9ab72d6323dd61c2d155b088148577b2
Rafael Souza <rafaeljosegomes25@gmail.com>
2022-10-13 17:37:21 -0300

    styled form inputs

--- src/screens/Checkout/index.tsx
+++ src/screens/Checkout/index.tsx
@@ -1,20 +1,44 @@
 import { Container } from '../../components/Container'
 import { CheckoutContainer, FormContainer, FormSubmitContainer } from './styles'
+import { MapPinLine } from 'phosphor-react'
 
 export function Checkout() {
   return (
     <Container>
-      <CheckoutContainer>
-        <FormContainer>
-          <span className="title">Complete seu pedido</span>
-          <div className="inputBox"></div>
-          <div className="paymentMethod"></div>
-        </FormContainer>
-        <FormSubmitContainer>
-          <span className="title">Cafés selecionados</span>
-          <div></div>
-        </FormSubmitContainer>
-      </CheckoutContainer>
+      <form action="">
+        <CheckoutContainer>
+          <FormContainer>
+            <span className="title">Complete seu pedido</span>
+            <div className="inputBox">
+              <div className="titleContainer">
+                <div className="titleBox">
+                  <MapPinLine size={24} color="#C47F17" />
+                  <strong>Endereço de Entrega</strong>
+                </div>
+                <span>Informe o endereço onde deseja receber seu pedido</span>
+              </div>
+              <div className="inputContainer">
+                <input type="text" placeholder="CEP" className="normal" />
+                <input type="text" placeholder="Rua" className="big" />
+                <input type="number" placeholder="Número" className="normal" />
+                <input
+                  type="text"
+                  placeholder="Complemento"
+                  className="optional"
+                />
+                <input type="text" placeholder="Bairro" className="normal" />
+                <input type="text" placeholder="Cidade" className="city" />
+                <input type="text" placeholder="UF" className="small" />
+              </div>
+            </div>
+            <div className="paymentMethod"></div>
+          </FormContainer>
+          <FormSubmitContainer>
+            <span className="title">Cafés selecionados</span>
+            <div></div>
+          </FormSubmitContainer>
+        </CheckoutContainer>
+      </form>
     </Container>
   )
 }
--- src/screens/Checkout/styles.ts
+++ src/screens/Checkout/styles.ts
@@ -37,6 +37,87 @@ export const FormContainer = styled.div`
         background-color: ${props.theme.baseCard};
       `}
   }
+
+  div.inputBox div.titleContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 0.125rem;
+  }
+
+  div.inputBox div.titleContainer div.titleBox {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  div.inputBox div.titleContainer div.titleBox strong {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 130%;
+  }
+
+  div.inputBox div.titleContainer span {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 130%;
+
+    margin-left: 2rem;
+  }
+
+  div.inputContainer {
+    margin-top: 2rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+  }
+
+  div.inputContainer input {
+    outline: 0;
+    height: 2.625rem;
+
+    margin-bottom: 0.5rem;
+
+    ${(props) =>
+      css`
+        background-color: ${props.theme.baseInput};
+        border: 1px solid ${props.theme.baseButton};
+      `}
+
+    border-radius: 4px;
+    padding: 0.75rem;
+
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 130%;
+  }
+
+  div.inputContainer input.normal {
+    width: 12.5rem;
+  }
+
+  div.inputContainer input.optional {
+    width: 21.75rem;
+    flex: 1;
+  }
+
+  div.inputContainer input.big {
+    width: 100%;
+  }
+
+  div.inputContainer input.small {
+    width: 3.75rem;
+    flex: 1;
+  }
+
+  div.inputContainer input.city {
+    width: 17.25rem;
+  }
 `
 
 export const FormSubmitContainer = styled.div`
